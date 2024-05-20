570f3248b48f25c3065873fc3624a072f844fc93
Melissa Johansson <mejo22@student.bth.se>
2024-05-20 19:51:43 +0200

    start testing get_recipe

--- backend/test/unit/test_solution.py
+++ backend/test/unit/test_solution.py
@@ -1,6 +1,28 @@
 import pytest
+from src.controllers.recipecontroller import RecipeController
+from unittest.mock import MagicMock
 
-# add your test case implementation here
+
+@pytest.fixture
+def recipe_controller():
+    mock_controller = MagicMock()
+    mock = RecipeController(mock_controller)
+    return mock
+
 @pytest.mark.unit
-def test():
+def test_get_recipe_true_readiness0point2(recipe_controller):
     pass
+
+@pytest.mark.unit
+def test_get_recipe_false_readiness0point2(recipe_controller):
+    pass
+
+@pytest.mark.unit
+def test_get_recipe_readiness0(recipe_controller):
+    pass
+
+@pytest.mark.unit
+def test_get_recipe_no_recipe_available(recipe_controller):
+    result = recipe_controller.get_recipe(MagicMock(), True)
+
+    assert result == None
